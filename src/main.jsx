98d35c3e235d3ff3b8b0b0eb5d86c4a3b438b57b
CasperLCasper <64712311+CasperLCasper@users.noreply.github.com>
2026-09-08 17:43:15 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './LanguageContext';
 import App from './App';
 import BackupPage from './BackupPage';
 import './styles.css';
@@ -8,10 +9,12 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/backup" element={<BackupPage />} />
-      </Routes>
+      <LanguageProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/backup" element={<BackupPage />} />
+        </Routes>
+      </LanguageProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
